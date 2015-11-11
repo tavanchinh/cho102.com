e5d126780ffc6636bfb2507dd075da764efc1a04
tavanchinh <chinh.tv91@gmail.com>
2015-11-11 15:51:59 +0700

    edit css/main.css

--- protected/views/layouts/widgets/distance.php
+++ protected/views/layouts/widgets/distance.php
@@ -6,24 +6,19 @@
  <div class="block-content">
     <ul class="list-distance">
        <li>
-          <i class="fa fa-hand-o-right"></i>
-          <a href="#">0 - 1km</a>
+          <a href="#"><i class="fa fa-hand-o-right"></i>0 - 1km</a>
        </li>
        <li>
-          <i class="fa fa-hand-o-right"></i>
-          <a href="#">1 - 2km</a>
+          <a href="#"><i class="fa fa-hand-o-right"></i>1 - 2km</a>
        </li>
        <li>
-          <i class="fa fa-hand-o-right"></i>
-          <a href="#">2 - 5km</a>
+          <a href="#"><i class="fa fa-hand-o-right"></i>2 - 5km</a>
        </li>
        <li>
-          <i class="fa fa-hand-o-right"></i>
-          <a href="#">5 - 10km</a>
+          <a href="#"><i class="fa fa-hand-o-right"></i>5 - 10km</a>
        </li>
        <li>
-          <i class="fa fa-hand-o-right"></i>
-          <a href="#">Trên 10km</a>
+          <a href="#"><i class="fa fa-hand-o-right"></i>Trên 10km</a>
        </li>
     </ul>
  </div>
